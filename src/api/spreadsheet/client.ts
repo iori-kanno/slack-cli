d60145996c93b4115dda0576de1dd5145673c558
iori-kanno <69029158+iori-kanno@users.noreply.github.com>
2024-08-22 00:07:06 +0900

    fix output order for aggregate:members-reacted (#43)
    * fix load file logic
    
    * fix output order for aggregate:members-reacted
    
    * update dist/

--- src/api/spreadsheet/client.ts
+++ src/api/spreadsheet/client.ts
@@ -1,17 +1,36 @@
+import fs from 'fs';
 import { Log } from '@/lib/log';
 import {
   GoogleSpreadsheet,
   ServiceAccountCredentials,
 } from 'google-spreadsheet';
 
-export const spreadSheetClient = (sheetId: string) => {
+export const spreadSheetClient = async (
+  sheetId: string
+): Promise<GoogleSpreadsheet | undefined> => {
   const doc = new GoogleSpreadsheet(sheetId);
-  try {
-    const credential = require('../../../.spreadsheet-credential.json');
-    doc.useServiceAccountAuth(credential as ServiceAccountCredentials);
-    return doc;
-  } catch (e) {
-    Log.error(`GoogleSpreadsheet Error: ${e}`);
-    return;
-  }
+  return new Promise((resolve, reject) => {
+    try {
+      fs.readFile(
+        `${process.cwd()}/` +
+          (process.env.GOOGLE_CREDENTIAL_FILE_PATH ||
+            '.spreadsheet-credential.json'),
+        (err, data) => {
+          if (err) {
+            Log.error(
+              `Tried to create a spreadsheet, but .spreadsheet-credential.json was not found in the project root.\nGoogleSpreadsheet Error: ${err}`
+            );
+            reject();
+            return;
+          }
+          const credential = JSON.parse(data.toString());
+          doc.useServiceAccountAuth(credential as ServiceAccountCredentials);
+          resolve(doc);
+        }
+      );
+    } catch (e) {
+      Log.error(`spreadSheetClient Error: ${e}`);
+      reject();
+    }
+  });
 };
